84dc5643d6cf687c2fe390abf7570b85d4bf0117
Joanna Wood <caitlinjoannawood@gmail.com>
2019-06-25 23:46:49 -0400

    account balance exercise complete

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ class Account {
   }
 
   get balance() {
-    let sumOfTransactions = 0;
+    let balance = 0;
     for (let transaction of this.transactions){
-      sumOfTransactions += transaction.value;
+      balance += transaction.value;
     }
-    return sumOfTransactions;    
+    return balance;    
   }
 
   addTransaction(transaction) {
@@ -23,19 +23,31 @@ class Transaction {
     this.account = account;
   }
 
-  commit(){
+  commit() {
+    if (!this.isAllowed()) return false;
     this.time = new Date();
     this.account.addTransaction(this);
+    return true;
   }
 }
 
 class Deposit extends Transaction {
+
+  isAllowed() {
+    return true;
+  }
+
   get value(){
     return this.amount;
   }
 }
 
 class Withdrawal extends Transaction {
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+
   get value() {
     return -this.amount;
   }
@@ -45,16 +57,24 @@ class Withdrawal extends Transaction {
 
 const myAccount = new Account('billybob');
 
-console.log('Starting Balance:', myAccount.balance);
+// console.log('Starting Balance:', myAccount.balance);
+
 
 t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
+// console.log('Transaction 1:', t1);
+console.log('After t1 balance:', myAccount.balance);
+
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+// console.log('Transaction 3:', t3);
+console.log('After t3 balance:', myAccount.balance);
 
 t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
+// console.log('Transaction 2:', t2);
+console.log('After t2 balance:', myAccount.balance);
 
-// t3 = new Deposit(120.00, myAccount);
-// t3.commit();
-// console.log('Transaction 3:', t3);
 
 console.log('Ending Balance:', myAccount.balance);
+// console.log('Transaction history:', myAccount.transactions);
